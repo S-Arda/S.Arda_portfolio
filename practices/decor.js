801cc8402592b676cbda478dfd83d7f600c9cad4
S-Arda <ardasher.saydulloev@gmail.com>
2025-10-26 10:09:11 +0500

    decor practices

---
+++ practices/decor.js
@@ -0,0 +1,27 @@
+function forsum(...args)
+{
+    return function sum1(...args)
+            {
+                let odd = 0 ;
+                for (let a = 0 ; a = args.length ; a++ ) 
+                {       
+                    if (args[a] % 2 == 0) 
+                    {
+                        odd = odd + args[a];
+                    }
+                }
+            }    
+}
+
+function sum() {
+    let i=0
+    let counter=0
+    while (i < arguments.length) {
+        counter+=arguments[i]
+        i++
+    }
+    return counter
+}
+
+let s = forsum(sum);
+console.log(s(1,2,3,4));
